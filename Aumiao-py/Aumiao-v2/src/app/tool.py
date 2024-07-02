e517d3be18b63a8b47c0a567ea7e9d4f0bd92431
zybqw <zybqw@qq.com>
2024-07-02 22:54:29 +0800

    updated

--- Aumiao-py/Aumiao-v2/src/app/tool.py
+++ Aumiao-py/Aumiao-v2/src/app/tool.py
@@ -1,4 +1,3 @@
-import json
 import time
 from typing import Any, Dict, List
 
@@ -44,35 +43,3 @@ class Process:
         for key in keys:
             value = value.get(key, {})
         return value
-
-
-class File:
-    # 从配置文件加载账户信息的函数
-    def file_load(self, path):
-        with open(path, "r", encoding="utf-8") as file:
-            data = json.load(file)
-            return data
-
-    # 检查文件
-    def check_file(self, path: str) -> bool:
-        try:
-            with open(path, "r"):
-                return True
-        except IOError:
-            return False
-
-    # 将文本写入到指定文件的函数
-    def write(
-        self,
-        path: str,
-        text: str | Dict,
-        type: str = "str",
-        method: str = "w",
-    ) -> None:
-        with open(path, mode=method, encoding="utf-8") as file:
-            if type == "str":
-                file.write(text + "\n")
-            elif type == "dict":
-                file.write(json.dumps(text, ensure_ascii=False, indent=4))
-            else:
-                raise ValueError("不支持的写入方法")
