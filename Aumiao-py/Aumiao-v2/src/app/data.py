8501970dc38b1534c3fabdddd5e061aa5e96bc73
zybqw <zybqw@qq.com>
2024-07-03 13:01:52 +0800

    update

--- Aumiao-py/Aumiao-v2/src/app/data.py
+++ Aumiao-py/Aumiao-v2/src/app/data.py
@@ -2,12 +2,13 @@ import os
 
 import src.app.file as File
 
+CONFIG_FILE_PATH: str = os.path.join(os.getcwd(), "data.json")
+
 
 class CodeMaoData:
-    CONFIG_FILE_PATH: str = os.path.join(os.getcwd(), "data.json")
 
     def __init__(self):
-        data = File.CodeMaoFile.file_load(self.CONFIG_FILE_PATH)
+        data = File.CodeMaoFile().file_load(CONFIG_FILE_PATH)
 
         self.PROGRAM_DATA.update(data["PROGRAM_DATA"])
         self.USER_DATA.update(data["USER_DATA"])
