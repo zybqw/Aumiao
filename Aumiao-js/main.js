24a2aeaa8e6f80b8736ce308e89d8ecbd69c2c56
MoonLeeeaf <150461955+MoonLeeeaf@users.noreply.github.com>
2024-07-06 09:25:15 +0800

    chore: Remove test code

--- Aumiao-js/main.js
+++ Aumiao-js/main.js
@@ -10,8 +10,4 @@ function run(f) {
     return f()
 }
 
-CodemaoApi.setCookie("authorization=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJDb2RlbWFvIEF1dGgiLCJ1c2VyX3R5cGUiOiJzdHVkZW50IiwiZGV2aWNlX2lkIjowLCJ1c2VyX2lkIjoxMTc3MDc2OCwiaXNzIjoiQXV0aCBTZXJ2aWNlIiwicGlkIjoiNjVlZENUeWciLCJleHAiOjE3MjMyOTgxMTAsImlhdCI6MTcxOTQxMDExMCwianRpIjoiYzk2NDY5ZmUtZWNmZS00ZDA5LTkwM2MtMWE3NzgyNWFmMGZkIn0.3BK4JoOdUbIR8xAYLzaNSjca6wM1PrrAR8jHR7EUwCQ")
 
-// TODO...
-
-run(async () => console.log(await CodemaoApi.Work.like(227158550)))
