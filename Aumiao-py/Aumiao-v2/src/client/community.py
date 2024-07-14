5829b4985e2360e0e4ebcd1f7a8cda6f0a903d28
zybqw <zybqw@qq.com>
2024-07-14 21:48:21 +0800

    add token login

--- Aumiao-py/Aumiao-v2/src/client/community.py
+++ Aumiao-py/Aumiao-v2/src/client/community.py
@@ -1,57 +1,94 @@
 import json
+import uuid
 from typing import Any, Dict, List, Optional
 
 import src.app.acquire as Acquire
 import src.app.data as Data
+import src.app.tool as Tool
 
 
 class Login:
     def __init__(self) -> None:
         self.acquire = Acquire.CodeMaoClient()
         self.data = Data.CodeMaoData()
+        self.tool_process = Tool.CodeMaoProcess()
 
-    # 登录函数, 处理登录逻辑并保存登录状态
-    def login(
+    # 密码登录函数
+    def login_password(
         self,
-        method: str = "password",
         identity: str | int = None,
         password: Any = None,
         pid: str = "65edCTyg",
-        cookies: Any = None,
     ) -> Optional[str]:
-        if method == "password":
-            # cookies = utils.dict_from_cookiejar(response.cookies)
 
-            #   soup = BeautifulSoup(
-            #       send_request("https://shequ.codemao.cn", "get").text,
-            #       "html.parser",
-            #   )
-            #   见https://api.docs.codemao.work/user/login?id=pid
-            #   pid = loads(soup.find_all("script")[0].string.split("=")[1])["pid"]
-            response = self.acquire.send_request(
-                url="/tiger/v3/web/accounts/login",
-                method="post",
-                data=json.dumps(
-                    {
-                        "identity": identity,
-                        "password": password,
-                        "pid": pid,
-                    }
-                ),
-            )
-        elif method == "cookie":
-            try:
-                dict([item.split("=", 1) for item in cookies.split("; ")])
-                # 检查是否合规,不能放到headers中
-            except (KeyError, ValueError) as err:
-                print(f"表达式输入不合法 {err}")
-                return False
-            response = self.acquire.send_request(
-                url="/nemo/v2/works/174408420/like",
-                method="post",
-                data=json.dumps({}),
-                headers={**self.data.PROGRAM_DATA["HEADERS"], "cookie": cookies},
-            )
+        # cookies = utils.dict_from_cookiejar(response.cookies)
+
+        #   soup = BeautifulSoup(
+        #       send_request("https://shequ.codemao.cn", "get").text,
+        #       "html.parser",
+        #   )
+        #   见https://api.docs.codemao.work/user/login?id=pid
+        #   pid = loads(soup.find_all("script")[0].string.split("=")[1])["pid"]
+        response = self.acquire.send_request(
+            url="/tiger/v3/web/accounts/login",
+            method="post",
+            data=json.dumps(
+                {
+                    "identity": identity,
+                    "password": password,
+                    "pid": pid,
+                }
+            ),
+        )
+        self.check_login(response)
+
+    # cookie登录
+    def login_cookie(self, cookies):
+
+        try:
+            dict([item.split("=", 1) for item in cookies.split("; ")])
+            # 检查是否合规,不能放到headers中
+        except (KeyError, ValueError) as err:
+            print(f"表达式输入不合法 {err}")
+            return False
+        response = self.acquire.send_request(
+            url="/nemo/v2/works/174408420/like",
+            method="post",
+            data=json.dumps({}),
+            headers={**self.data.PROGRAM_DATA["HEADERS"], "cookie": cookies},
+        )
+        self.check_login(response)
+
+    # token登录(毛毡最新登录方式)
+    def login_token(self, identity: str | int = None, password: Any = None):
+        timestamp = Obtain().get_timestamp()["data"]
+        response = self.login_v3(identity=identity, timestamp=timestamp)
+        ticket = response["ticket"]
+        response = self.login_security(
+            identity=identity, password=password, ticket=ticket
+        )
+        token = response["auth"]["token"]
+        # src.client_community_login.logout()
+        # response = src.app_acquire.send_request(url="https://shequ.codemao.cn/",method="get",)
+        # aliyungf_tc = response.cookies.get_dict()["aliyungf_tc"]
+        uuid_ca = uuid.uuid1()
+        token_ca = {"authorization": token, "__ca_uid_key__": str(uuid_ca)}
+        cookie_str = self.tool_process.process_cookie(token_ca)
+        headers = {**self.data.PROGRAM_DATA["HEADERS"], "cookie": cookie_str}
+        response = self.acquire.send_request(
+            method="get", url="/web/users/details", headers=headers
+        )
+        _auth = response.cookies.get_dict()
+        auth_cookie = {**token_ca, **_auth}
+        cookie_str = self.tool_process.process_cookie(auth_cookie)
+        headers = {**self.data.PROGRAM_DATA["HEADERS"], "cookie": cookie_str}
+        response = self.acquire.send_request(
+            method="get", url="/web/users/details", headers=headers
+        )
+        self.check_login(response)
+
+    # 检查并保存登录状态
+    def check_login(self, response):
         if response.status_code == 200:
             self.acquire.update_cookie(response.cookies)  # 确保cookies被更新
             return True
@@ -93,6 +130,7 @@ class Login:
         )
         return response.json()
 
+    #
     # 登录ticket获取
     def login_v3(
         self,
