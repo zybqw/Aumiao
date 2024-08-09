f4e1676c6be958fe12909d35cd9b9093fecf8f57
zybqw <zybqw@qq.com>
2024-08-09 15:46:32 +0800

    setting预设，login登录优化

--- Aumiao-py/src/app/data.py
+++ Aumiao-py/src/app/data.py
@@ -12,20 +12,8 @@ SETTING_FILE_PATH: str = os.path.join(os.getcwd(), "data/" "setting.json")
 class CodeMaoData:
     def __init__(self) -> None:
         data = File.CodeMaoFile().file_load(path=DATA_FILE_PATH, type="json")
-
-        self.PROGRAM_DATA.update(data["PROGRAM_DATA"])  # type: ignore
         self.USER_DATA.update(data["USER_DATA"])  # type: ignore
         self.ACCOUNT_DATA.update(data["ACCOUNT_DATA"])  # type: ignore
-
-    PROGRAM_DATA = {
-        "HEADERS": {
-            "Content-Type": "",
-            "User-Agent": "",
-        },
-        "BASE_URL": "",
-        "SLOGAN": "",
-        "SAVE_PATH": "",
-    }
 
     USER_DATA = {
         "black_room": {
@@ -48,3 +36,40 @@ class CodeMaoData:
         "create_time": "",
         "author_level": "",
     }
+
+
+@singleton
+class CodeMaoSetting:
+    def __init__(self) -> None:
+        data = File.CodeMaoFile().file_load(path=SETTING_FILE_PATH, type="json")
+        self.PROGRAM.update(data["PROGRAM"])  # type: ignore
+        self.PARAMETER.update(data["PARAMETER"])  # type: ignore
+        self.PLUGIN.update(data["PLUGIN"])  # type: ignore
+        self.DEFAULT = data["DEFAULT"]  # type: ignore
+
+    PROGRAM = {
+        "HEADERS": {
+            "Content-Type": "",
+            "User-Agent": "",
+        },
+        "BASE_URL": "",
+        "SLOGAN": "",
+    }
+    PARAMETER = {
+        "APP": {},
+        "CLIENT": {
+            "cookie_check_url": "",
+            "get_works_method": "",
+            "all_read_type": [],
+            "clear_ad_exclude_top": "",
+            "password_login_method": "",
+        },
+    }
+    PLUGIN = {
+        "prompt": "",
+        "DASHSCOPE": {
+            "model": "",
+            "more": {"stream": "", "extra_body": {"enable_search": ""}},
+        },
+    }
+    DEFAULT = [{"name": "", "action": ""}]
