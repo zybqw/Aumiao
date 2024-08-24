a867d58f21b1fc82093d3ebc16a89b0989b78b3f
zybqw <zybqw@qq.com>
2024-08-24 09:57:04 +0800

    api增加，代码优化

--- Aumiao-py/src/client/post.py
+++ Aumiao-py/src/client/post.py
@@ -40,6 +40,7 @@ class Obtain:
         )
         return replies
 
+    # 获取我的帖子或回复的帖子
     def get_post_mine(
         self,
         method: Literal["created", "replied"],
@@ -48,10 +49,62 @@ class Obtain:
     ):
         params = {"page": page, "limit": limit}
         posts = self.acquire.fetch_all_data(
-            url=f"https://api.codemao.cn/web/forums/posts/mine/{method}",
+            url=f"/web/forums/posts/mine/{method}",
             params=params,
             data_key="items",
             method="page",
             args={"amount": "limit", "remove": "page"},
         )
         return posts
+
+    # 获取论坛帖子各个栏目
+    def get_post_categories(self):
+        response = self.acquire.send_request(
+            url="/web/forums/boards/simples/all", method="get"
+        )
+        return response.json()
+
+    # 获取社区所有热门帖子
+    def get_hot_posts(self):
+        response = self.acquire.send_request(
+            url="/web/forums/posts/hots/all", method="get"
+        )
+        return response.json()
+
+    # 获取论坛顶部公告
+    def get_top_notice(self, limit: int = 4):
+        params = {"limit": limit}
+        response = self.acquire.send_request(
+            url="/web/forums/notice-boards", method="get", params=params
+        )
+        return response.json()
+
+    # 获取论坛本周精选帖子 TODO: 待完善
+    def get_key_content(
+        self, content_key: Literal["forum.index.top.recommend"], limit: int = 4
+    ):
+        params = {"content_key": content_key, "limit": limit}
+        response = self.acquire.send_request(
+            url="/web/contents/get-key", method="get", params=params
+        )
+        return response.json()
+
+    # 获取社区精品合集帖子
+    def get_selection_posts(self, limit: int = 20, offset: int = 0):
+        params = {"limit": limit, "offset": offset}
+        response = self.acquire.send_request(
+            url="/web/forums/posts/selections",
+            method="get",
+            params=params,
+        )
+        return response.json()
+
+    # 获取社区求帮助帖子
+    def get_help_posts(self, limit: int = 20, page: int = 1):
+        params = {"limit": limit, "page": page}
+        response = self.acquire.send_request(
+            url="/web/forums/boards/posts/ask-help",
+            method="get",
+            params=params,
+        )
+        return response.json()
